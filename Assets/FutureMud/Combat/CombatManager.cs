b8a6efbf2e7b0b148125ff61ef78f3144bfcda1e
Filip <filip@sigrand.se>
2020-12-21 11:28:09 +0100

    before merge with editor changes

---
+++ Assets/FutureMud/Combat/CombatManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FutureMud.Actor;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    public static CombatManager instance;
+    private Actor[] playerTeam;
+    private Actor[] enemyTeam;
+    
+    
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void InitCombat(Actor[] playerTeam, Actor[] enemyTeam)
+    {
+        this.playerTeam = playerTeam;
+        this.enemyTeam = enemyTeam;
+        
+        
+    }
+
+    private void RollTurnOrder()
+    {
+        
+    }
+    
+    
+
+    public void EndCombat()
+    {
+        
+    }
+}
---
+++ Assets/FutureMud/Combat/CombatUIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject combatUI;
+    [SerializeField] private Image combatUIPanel;
+    [SerializeField] private Text combatLogText;
+    
+    public void CombatUiSetActive(bool activeState)
+    {
+        combatUI.SetActive(activeState);
+    }
+}
